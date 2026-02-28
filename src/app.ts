ab75cfae88ad9cc92938147d878f19f78bef6da1
Taran Badesha <tsingh18@rrc.ca>
2026-02-27 23:57:47 -0600

    added server lisetn

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Express } from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+import healthRoutes from "./api/v1/routes/healthRoutes";
+import eventRoutes from "./api/v1/routes/eventRoutes";
+
+const app: Express = express();
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use("/api/v1/health", healthRoutes);
+app.use("/api/v1/events", eventRoutes);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./app";
+
+const PORT: number = Number(process.env.PORT) || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
