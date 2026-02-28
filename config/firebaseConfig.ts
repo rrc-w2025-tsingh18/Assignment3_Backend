03aef8acda72349e32b737b11ec77b834af23a39
Taran Badesha <tsingh18@rrc.ca>
2026-02-28 17:32:05 -0600

    configure firebase admin and update tsconfig

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,8 +1,11 @@
 import admin from "firebase-admin";
 import { getFirestore } from "firebase-admin/firestore";
+import serviceAccount from "./serviceAccountKey.json";
 
 if (!admin.apps.length) {
-  admin.initializeApp();
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
 }
 
 export const db = getFirestore();
