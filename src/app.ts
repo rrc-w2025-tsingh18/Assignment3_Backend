992752b7739babb47842f87f89a64a1b110ab964
Taran Badesha <tsingh18@rrc.ca>
2026-03-25 10:13:46 -0500

    addd the custom helmet configuration for api protection

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import morgan from "morgan";
+import helmet from "helmet";
 
 dotenv.config();
 
@@ -9,6 +10,14 @@ import healthRoutes from "./api/v1/routes/healthRoutes";
 import eventRoutes from "./api/v1/routes/eventRoutes";
 
 const app: Express = express();
+
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginResourcePolicy: { policy: "cross-origin" },
+    referrerPolicy: { policy: "no-referrer" },
+  })
+);
 
 app.use(cors());
 app.use(morgan("dev"));
